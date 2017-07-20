408d75b95574393ac95f5c44253f9a1dfb8d9718
sangeethamano <sangeethamanoharanm@gmail.com>
2017-07-20 18:16:46 +0530

    Add files via upload

---
+++ sc.c
@@ -0,0 +1,14 @@
+    #include <stdio.h>
+    int main(void)
+    {
+    	int n;
+    	printf("enter the number");
+    	scanf("%d",&n);
+    	if(n>0)
+    	printf("positive\n");
+    	else if(n=0)
+    	printf("the number is neither positive nor negative\n");
+    	else
+    	printf("negative\n");
+
+  }
