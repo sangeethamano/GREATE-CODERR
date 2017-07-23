e09f153cce7d7b8123a60cba396b2ac3f21d651c
sangeethamano <sangeethamanoharanm@gmail.com>
2017-07-23 14:20:39 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int base,exponent;
+long long result=1;
+printf("enter the base number");
+scanf("%d",&base);
+printf("enter the exponent");
+scanf("%d",&exponent);
+while(exponent!=0)
+{
+result=result*base;
+--exponent;
+}
+printf("ans=%lld",result);
+return 0;
+}
+
