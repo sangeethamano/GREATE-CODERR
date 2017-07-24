b594ba9805544980704999ef77727cbcdf7eaa3c
sangeethamano <sangeethamanoharanm@gmail.com>
2017-07-24 13:04:36 +0530

    Add files via upload

---
+++ oddnum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+    int num,m,n;
+
+    printf("Print Odd Numbers in a given range m to n:\n");
+scanf("%d%d",&m,&n);
+
+    for (num = m; num <= n; num++)
+
+        {
+
+               if (num % 2 == 1)
+
+                  printf ("%d ", num);
+
+         }
+
+} 
